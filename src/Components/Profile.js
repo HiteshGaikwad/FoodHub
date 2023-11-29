bf959401970085409b2050b40b292aeb950fbc99
Hitesh <gaikwadhitesh03@gmail.com>
2023-11-30 01:11:37 +0530

    changes made

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -26,12 +26,12 @@ class Profile extends React.Component{
             <div className="flex flex-col gap-4 sm:gap-7 sm:w-2/5 sm:justify-center">
             <h1 className="sm:text-5xl text-2xl font-bold sm:font-semibold">{this.state.profileInfo?.name}</h1>
             <div className="flex gap-5 sm:justify-between ">
-                <h2 className="sm:text-xl text-md font-semibold">Software Developer</h2>
+                <h2 className="sm:text-xl text-md font-semibold">Software Engineer</h2>
                 <h2 className="sm:text-xl text-md sm:mr-40 font-semibold">{this.state.profileInfo?.location}</h2>
             </div>
             <div className="sm:text-xl text-sm font-normal sm:font-bold sm:mx-6 italic">
-            <p>"I am a Software Developer, and I believe creating something new everyday will help you to learn alot.</p>
-             <p>I have passion for Software Developement, but on the other side, I also enjoy developing something new."</p>
+            <p>"I am a Software Engineer, and I believe creating something new everyday will help you to learn alot.</p>
+             <p>I have passion for Software Developement, but on the other side, I enjoy developing something new."</p>
              </div>
             </div>
             </div>
